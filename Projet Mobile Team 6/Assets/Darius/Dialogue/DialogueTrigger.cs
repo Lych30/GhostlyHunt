37c238c837307f43c2f48a839676b8e3ffc62dc1
Leo <Aqu4men@gmail.com>
2021-06-14 10:41:54 +0200

    fix dialogue z et focus

--- Projet Mobile Team 6/Assets/Darius/Dialogue/DialogueTrigger.cs
+++ Projet Mobile Team 6/Assets/Darius/Dialogue/DialogueTrigger.cs
@@ -9,13 +9,31 @@ public class DialogueTrigger : MonoBehaviour
     public bool dialogue_Charlotte;
     public bool dialogue_Fantome;
 
+    public bool focus;
+    private bool Triggerfocus;
+    private Vector3 refvel;
+    public GameObject FocusGameobject;
+
+    private void Update()
+    {
+        if (Triggerfocus)
+        {
+            Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, FocusGameobject.transform.position, ref refvel, 3, 3);
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y,-10);
+        }
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             TriggerDialogue();
+            if (focus)
+            {
+                Triggerfocus = true;
+            }
         }
     }
+
 
     public void TriggerDialogue()
     {
